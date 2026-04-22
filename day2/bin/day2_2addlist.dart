cd4287341a50d3094d2cf713d7cbee95f21d09ad
MuhammadRahman2 <muhammadrahman2042@gmail.com>
2026-04-22 14:35:22 +0500

    day2 learn variable

---
+++ day2/bin/day2_2addlist.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> hobbies = ["Coding", "Cricket", "Learning"];
+
+  print("My hobbies:");
+  for (var hobby in hobbies) {
+    print(hobby);
+  }
+
+  // or
+//   print("My hobbies:");
+//   for (var i= 0; i < hobbies.length; i++) {
+//     print(hobbies[i]);
+//   }
+}
---
+++ day2/bin/day2_3addmap.dart
@@ -0,0 +1,10 @@
+void main() {
+  Map<String, dynamic> user = {
+    "name": "Rahman",
+    "age": 24,
+    "isStudent": false
+  };
+
+  print(user["name"]);
+  print(user["age"]);
+}
