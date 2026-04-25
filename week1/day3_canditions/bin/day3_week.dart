08dac0f72b85f61e12dcc09bb3353a7d11fa7896
MuhammadRahman2 <muhammadrahman2042@gmail.com>
2026-04-25 09:50:34 +0500

    week 1 and day 3 done

---
+++ week1/day3_canditions/bin/day3_week.dart
@@ -0,0 +1,14 @@
+void main(){
+  String day = "Monday";
+
+switch (day) {
+  case "Monday":
+    print("Start work");
+    break;
+  case "Friday":
+    print("Weekend coming");
+    break;
+  default:
+    print("Normal day");
+}
+}
